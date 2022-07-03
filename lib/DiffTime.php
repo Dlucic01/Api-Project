540b83e95606e4512564a48bb5adc9667ae688bd
Dlucic01 <david.lucic01@gmail.com>
2022-07-03 19:52:38 +0200

    Diff Time Fix

--- lib/DiffTime.php
+++ lib/DiffTime.php
@@ -8,17 +8,23 @@ class DiffTime
      * Validate diff_time parameter
      */
 
-    public static function validDiffTime(): bool
+    public static function validDiffTime()
     {
-        if (isset($_GET['diff_time'])
-        && (int) $_GET['diff_time'] <= 0
-        || (int) $_GET['diff_time'] > time()
+
+        $diffTime = $_GET['diff_time'];
+        $diffTime = gmdate("Y-m-d H:i:s", $diffTime);
+
+        if (
+            isset($diffTime)
+            && (int) $diffTime <= 0
+            || (int) $diffTime > time()
         ) {
-            echo "Diff time can't be <= 0 or > current time()";
+            echo "Diff time can't be <= 0 or > current UNIX time";
             return false;
         } elseif (!isset($_GET['diff_time'])) {
+
             return false;
         }
-        return true;
+        return $diffTime;
     }
 }
